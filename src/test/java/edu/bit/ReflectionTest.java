7834f49e5a3571e46a72f553ad87e57a26f8e075
Naman <namannigam12@gmail.com>
2021-01-29 16:14:08 +0530

    test getting the most specific method

--- src/test/java/edu/bit/ReflectionTest.java
+++ src/test/java/edu/bit/ReflectionTest.java
@@ -4,6 +4,8 @@ package edu.bit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class ReflectionTest {
 
     @Test
@@ -12,5 +14,47 @@ class ReflectionTest {
                 .getAnnotation(Reflection.AnnotationParsingUsingReflection.Table.class).name());
         Assertions.assertEquals("attack_type", Reflection.AnnotationParsingUsingReflection.Jedi.class.getField("attackType")
                 .getAnnotation(Reflection.AnnotationParsingUsingReflection.Column.class).name());
+    }
+
+    static class Foo extends Bar {
+    }
+
+    abstract static class Bar {
+    }
+
+    interface ServiceA<S> {
+        S getType();
+    }
+
+    interface ServiceB {
+
+        @Deprecated
+        Bar getType();
+    }
+
+    static class ServiceImpl implements ServiceX {
+        @Override
+        public Foo getType() {
+            return null;
+        }
+    }
+
+    interface ServiceX extends ServiceA<Foo>, ServiceB {
+    }
+
+    @Test
+    void testGettingTheMostSpecificMethod() {
+        ServiceImpl service = new ServiceImpl();
+        for (Method method : service.getClass().getMethods()) {
+            for (Class<?> anInterface : method.getDeclaringClass().getInterfaces()) {
+                try {
+                    Method interpretedMethod = anInterface.getMethod(method.getName(), method.getParameterTypes());
+                    System.out.println("interpretedMethod = " + interpretedMethod);
+                    Assertions.assertEquals(ServiceB.class, interpretedMethod.getDeclaringClass());
+                    Assertions.assertEquals(Bar.class, interpretedMethod.getReturnType());
+                } catch (NoSuchMethodException e) {
+                }
+            }
+        }
     }
 }
