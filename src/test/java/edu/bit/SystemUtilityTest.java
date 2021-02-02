8b0c5949a22ac27006a7b0be64f87220cba39a8e
Naman <namannigam12@gmail.com>
2021-02-02 22:48:53 +0530

    test accessing preview classes

--- src/test/java/edu/bit/SystemUtilityTest.java
+++ src/test/java/edu/bit/SystemUtilityTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.bit;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
 class SystemUtilityTest {
-  
+
+    @Test
+    void testAccessingPreviewClasses() throws IOException {
+        FileSystem fs = FileSystems.getFileSystem(URI.create("jrt:/"));
+        // Object works, I get the bytes.
+        byte[] object = Files.readAllBytes(fs.getPath("modules", "java.base",
+                "java/lang/Object.class"));
+        // Record fails, NoSuchFile.
+        byte[] record = Files.readAllBytes(fs.getPath("modules", "java.base",
+                "java/lang/Record.class"));
+    }
+
 }
