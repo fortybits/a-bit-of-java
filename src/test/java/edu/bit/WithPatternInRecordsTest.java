2ae47a2fa9fb832fe135761eed79d6e5e4c10248
Naman <namannigam12@gmail.com>
2021-02-04 22:21:25 +0530

    test the pattern created

--- src/test/java/edu/bit/WithPatternInRecordsTest.java
+++ src/test/java/edu/bit/WithPatternInRecordsTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.bit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class WithPatternInRecordsTest {
-  
+
+    @Test
+    void testWithPatternInRecords() {
+        WithPatternInRecords.Vehicle vehicle = new WithPatternInRecords.Vehicle("Subaru", "ABC-DEFG");
+        WithPatternInRecords.Vehicle ultraVehicle = vehicle.withLicense("LMN-OPQR");
+        WithPatternInRecords.Vehicle premierVehicle = vehicle.withBrand("Umanga");
+
+        Assertions.assertEquals(vehicle.brand(), ultraVehicle.brand());
+        Assertions.assertNotEquals(vehicle.brand(), premierVehicle.brand());
+        Assertions.assertEquals(vehicle.brand(), ultraVehicle.brand());
+        Assertions.assertNotEquals(vehicle.licensePlate(), premierVehicle.licensePlate());
+    }
 }
