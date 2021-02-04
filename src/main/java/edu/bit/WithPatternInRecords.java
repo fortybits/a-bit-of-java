a94afe96db25f8bd075ed24f8b455730f57a0b3e
Naman <namannigam12@gmail.com>
2021-02-04 22:22:49 +0530

    references

--- src/main/java/edu/bit/WithPatternInRecords.java
+++ src/main/java/edu/bit/WithPatternInRecords.java
@@ -1,5 +1,9 @@
 package edu.bit;
 
+/**
+ * Problem statement: Make use of 'with' in records.
+ * Details: https://stackoverflow.com/questions/66048957/do-java-records-support-with-syntax
+ */
 public class WithPatternInRecords {
     public record Vehicle(String brand, String licensePlate) {
         Vehicle withLicense(String licensePlate) {
