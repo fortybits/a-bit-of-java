50c357f19fdfec16ab197e62125e264b60de277f
Naman <namannigam12@gmail.com>
2021-02-18 23:53:54 +0530

    efficient manner to remove duplicates on files

--- src/main/java/edu/bit/RemoveDuplicatesEmails.java
+++ src/main/java/edu/bit/RemoveDuplicatesEmails.java
@@ -1,18 +1,14 @@
 package edu.bit;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Set;
-import java.util.LinkedHashSet;
-import java.util.Random;
-import java.util.List;
-import java.util.Collection;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class RemoveDuplicatesEmails {
@@ -65,6 +61,31 @@ public class RemoveDuplicatesEmails {
         writer.write(finalText);
         writer.close();
         return fileName;
+    }
+
+    // this method took 24s to process a 75GB file with a 10% clutter of duplicates and reduce it to 67GB output
+    public void deDuplicateEfficiently() throws IOException {
+        final String inputFileName = "uIdHash.txt";
+        final String outputFileName = "distinctUIdHash.txt";
+        File outputFile = new File(outputFileName);
+        boolean newFile = outputFile.createNewFile();
+        System.out.printf("%s new file created: %b\n", outputFileName, newFile);
+
+        long startTime = System.currentTimeMillis();
+
+        Set<String> distinctEmails = new HashSet<>();
+        try (FileInputStream inputStream = new FileInputStream(inputFileName);
+             Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8.name());
+             FileWriter writer = new FileWriter(outputFileName)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (distinctEmails.add(line)) {
+                    writer.write(line);
+                    writer.append("\n");
+                }
+            }
+        }
+        System.out.println("Time taken to process the data in milliseconds:" + (System.currentTimeMillis() - startTime));
     }
 
 }
