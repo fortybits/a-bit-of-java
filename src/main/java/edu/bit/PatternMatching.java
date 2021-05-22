8e5cd71fb71b6d67eea918da2b6e19bb5411f510
Naman <namannigam12@gmail.com>
2021-05-22 15:04:40 +0530

    patern matching

--- src/main/java/edu/bit/PatternMatching.java
+++ src/main/java/edu/bit/PatternMatching.java
@@ -55,4 +55,63 @@ public class PatternMatching {
 //            default:        format = obj.toString();
 //        }
     }
+
+    class Shape {
+    }
+
+    class Rectangle extends Shape {
+    }
+
+    class Triangle extends Shape {
+        int calculateArea() {
+            return 1;
+        }
+    }
+
+//    static void testTriangle(Shape s) {
+//        switch (s) {
+//            case null:
+//                break;
+//            case Triangle t:
+//                System.out.println("Triangle :)");
+//                if (t.calculateArea() > 100) {
+//                    System.out.println("Oh, Large triangle!!");
+//                }
+//                break;
+//            default:
+//                System.out.println("Non-triangle");
+//        }
+//    }
+//
+//    static void testTriangle(Shape s) {
+//        switch (s) {
+//            case Triangle t ->
+//                System.out.println("Triangle :)"); // Q. the order matters? not really an if..else per say!!
+//            case Triangle t && (t.calculateArea() > 100) ->
+//                System.out.println("Oh, Large triangle!!");
+//            default -> System.out.println("Non-triangle");
+//        }
+//    }
+
+    /**
+     *
+     * The compiler checks all pattern labels. It is a compile-time error if a pattern label in a
+     * switch block is dominated by an earlier pattern label in that switch block.
+     * (For this purpose, case null, T t is treated as if it were case T t.)
+     */
+
+    /**
+     * It is also a compile-time error if a switch block has more than one match-all switch label.
+     * The two match-all switch labels are default and case null, default.
+     */
+
+    /**
+     * If the type of the selector expression is a sealed class (JEP 397),
+     * then the type coverage check can take into account the permits clause of the sealed class
+     * to determine whether a switch block is complete.
+     * To defend against incompatible separate compilation, the compiler automatically adds a default label
+     * whose code throws an IncompatibleClassChangeError.
+     * This label will only be reached if the sealed interface is changed and the switch code is not recompiled.
+     * In effect, the compiler hardens your code for you.
+     */
 }
