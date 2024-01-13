f02a4a382f27b0fcc8989ce13e47c2f934e74188
Naman Nigam <namannigam12@gmail.com>
2024-01-13 08:49:12 +0530

    JDK21 Virtual Threads and Non Sealed classes

--- src/main/java/edu/bit/Sealed.java
+++ src/main/java/edu/bit/Sealed.java
@@ -1,13 +1,32 @@
 package edu.bit;
 
 import java.io.Serializable;
+import java.lang.reflect.Modifier;
 
 public class Sealed {
+
+    sealed interface Shape permits Rectangle, Square {
+    }
+
+    record Rectangle() implements Shape {
+    }
+
+    record Square() implements Shape {
+    }
+
+    public static void main(Shape shape) {
+        switch (shape) {
+            case Rectangle r -> System.out.println(r);
+            case Square sq -> System.out.println(sq);
+            default -> System.out.println("Def");
+        }
+    }
 
     public void sealedExperiments() {
         Integral integral = new Integral();
         integral.bar();
         integral.foo();
+
     }
 
     public sealed interface Int permits Some {
@@ -15,6 +34,7 @@ public class Sealed {
 
         void bar();
     }
+
 
     sealed interface Node {
         record ConstNode(int i) implements Node {
@@ -65,4 +85,67 @@ public class Sealed {
 //        };
 //    }
 
+    public static boolean isExplicitlyNonSealed(Class<?> clazz) {
+        boolean isClassSealed = clazz.isSealed();
+        boolean isSuperSealed = clazz.getSuperclass() != null && clazz.getSuperclass().isSealed();
+        boolean isAnyInterfaceSealed = false;
+        for (Class<?> interfaceClass : clazz.getInterfaces()) {
+            if (interfaceClass.isSealed()) {
+                isAnyInterfaceSealed = true;
+                break;
+            }
+        }
+        return (!isClassSealed && (isSuperSealed || isAnyInterfaceSealed)) && !Modifier.isFinal(clazz.getModifiers());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("isExplicitlyNonSealed(NonSealedClass.class) = " + isExplicitlyNonSealed(NonSealedClass.class));
+        System.out.println("isExplicitlyNonSealed(SealedClass.class) = " + isExplicitlyNonSealed(SealedClass.class));
+        System.out.println("isExplicitlyNonSealed(FinalClass.class) = " + isExplicitlyNonSealed(FinalClass.class));
+        System.out.println("isExplicitlyNonSealed(OrdinaryClass.class) " + isExplicitlyNonSealed(OrdinaryClass.class));
+        System.out.println("isExplicitlyNonSealed(FinalSealedClass.class) = " + isExplicitlyNonSealed(FinalSealedClass.class));
+        System.out.println("isExplicitlyNonSealed(SubSealedClass.class) = " + isExplicitlyNonSealed(SubSealedClass.class));
+        System.out.println("isExplicitlyNonSealed(SubSubClass.class) = " + isExplicitlyNonSealed(SubSubClass.class));
+        System.out.println("isExplicitlyNonSealed(ImplementingClass.class) = " + isExplicitlyNonSealed(ImplementingClass.class));
+        System.out.println("isExplicitlyNonSealed(Something.class) = " + isExplicitlyNonSealed(Something.class));
+        System.out.println("isExplicitlyNonSealed(ConstantExpr.class) = " + isExplicitlyNonSealed(ConstantExpr.class));
+    }
+
+    sealed class SealedClass permits NonSealedClass, FinalSealedClass, SubSealedClass {
+    }
+
+    sealed interface Expr
+            permits ConstantExpr{
+        public int eval();
+    }
+
+    record ConstantExpr(int i) implements Expr {
+        public int eval() { return i(); }
+    }
+
+    non-sealed class NonSealedClass extends SealedClass {
+    }
+
+    final class FinalSealedClass extends SealedClass {
+    }
+
+    sealed class SubSealedClass extends SealedClass permits SubSubClass {
+    }
+
+    non-sealed class SubSubClass extends SubSealedClass {
+    }
+
+    class Something extends SubSubClass {}
+
+    final class FinalClass {
+    }
+
+    class OrdinaryClass {
+    }
+
+    sealed interface SealedInterface permits ImplementingClass {
+    }
+
+    non-sealed class ImplementingClass implements SealedInterface {
+    }
 }
--- src/main/java/edu/bit/advanced/loom/VirtualThreads.java
+++ src/main/java/edu/bit/advanced/loom/VirtualThreads.java
@@ -1,12 +1,14 @@
 package edu.bit.advanced.loom;
 
 import java.time.Duration;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class VirtualThreads {
 
-    public static void main(String[] args) {
+    // virtual threads are scheduled by a global scheduler with as many workers as there are CPU cores
+    // or as explicitly set with -Djdk.defaultScheduler.parallelism=N
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
             IntStream.range(0, 10_000).forEach(i -> {
                 executor.submit(() -> {
@@ -15,5 +17,14 @@ public class VirtualThreads {
                 });
             });
         }  // executor.close() is called implicitly, and waits
+
+        System.out.println("Before a virtual thread! In - " + Thread.currentThread().threadId());
+        Thread.ofVirtual().start(() -> System.out.println("Inside of a virtual thread! In - " + Thread.currentThread().threadId()));
+        System.out.println("After a virtual thread! In - " + Thread.currentThread().threadId());
+
+        ThreadFactory tf = Thread.ofVirtual().factory();
+        ExecutorService e = Executors.newThreadPerTaskExecutor(tf);
+        Future<String> f = e.submit(() -> "Naman"); // spawns a new virtual thread
+        String y = f.get(); // joins the virtual thread
     }
 }
