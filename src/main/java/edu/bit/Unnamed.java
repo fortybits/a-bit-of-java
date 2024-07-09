fad553904af9badaa4d58a7fa955006b1bfb0946
Naman Nigam <namannigam12@gmail.com>
2024-07-09 22:25:31 +0530

    Work with exec:java

---
+++ src/main/java/edu/bit/Unnamed.java
@@ -0,0 +1,9 @@
+package edu.bit;
+
+public class Unnamed {
+
+    public static void main(String[] args) {
+        char[] _ = { 'b', 'o', 'b' };
+        System.out.println("Its Working!");
+    }
+}
